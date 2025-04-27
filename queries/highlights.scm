59214311df4df7d38e1230c9bb42844f213760aa
Jon Mease <jonmmease@gmail.com>
2025-04-26 20:03:12 -0400

    update highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -7,33 +7,42 @@
 
 (literal) @string
 
+;; SQL Keywords
 (keyword_in) @keyword.operator
 (keyword_and) @keyword.operator
 (keyword_or) @keyword.operator
 (keyword_not) @keyword.operator
 (keyword_by) @keyword.operator
 (keyword_on) @keyword.operator
-
 (keyword_select) @keyword
 (keyword_from) @keyword
 (keyword_where) @keyword
 (keyword_values) @keyword
 
+;; SQL operators
+(is_not) @keyword.operator
+(not_like) @keyword.operator
+(similar_to) @keyword.operator
+(not_similar_to) @keyword.operator
+(distinct_from) @keyword.operator
+(not_distinct_from) @keyword.operator
+(not_in) @keyword.operator
+(keyword_between) @keyword.operator
+
 ;; Function-related keywords
 (keyword_fn) @keyword
 (keyword_return) @keyword
-(function_def "->" @operator)
 
 ;; Explicitly highlight function return kind
 (function_def
-  "->"
-  "val" @keyword.storage.type)
+  "->" @operator
+  return_kind: (keyword_val) @keyword.storage.type)
 (function_def
-  "->"
-  "expr" @keyword.storage.type)
+  "->" @operator
+  return_kind: (keyword_expr) @keyword.storage.type)
 (function_def
-  "->"
-  "dataset" @keyword.storage.type)
+  "->" @operator
+  return_kind: (keyword_dataset) @keyword.storage.type)
 
 ;; Function definition
 (function_def
@@ -43,16 +52,14 @@
 (parameter_decl
   kind: _ @keyword.storage.type)
 
-;; Function return type
-(function_def
-  return_kind: _ @keyword.storage.type)
-
+;; Conditionals
 (keyword_case) @conditional
 (keyword_when) @conditional
 (keyword_then) @conditional
 (keyword_else) @conditional
+(keyword_end) @conditional
 
-;; Variable references (like @pi)
+;; Variable references and parameters (including the three different forms: ?, $n, and @var)
 (parameter) @variable.special
 
 ;; Table references with variable (like @data_0 in FROM @data_0)
@@ -65,15 +72,24 @@
 (pascal_identifier) @type
 
 ;; Property type keywords with distinct stylings
-(expr_prop "expr" @keyword.storage.type)
-(val_prop "val" @keyword.storage.type)
-(dataset_prop "dataset" @keyword.storage.type)
-(comp_prop "comp" @keyword.storage.type)
+(expr_prop
+  (keyword_expr) @keyword.storage.type)
+(val_prop
+  (keyword_val) @keyword.storage.type)
+(dataset_prop
+  (keyword_dataset) @keyword.storage.type)
+(comp_prop
+  (keyword_comp) @keyword.storage.type)
 
 ;; Component definition
 (component_def
   "component" @keyword
   "inherits" @keyword)
+
+;; Import statement
+(keyword_import) @keyword
+(import_path "from" @keyword)
+(import_item "as" @keyword)
 
 ;; Type annotation
 (type) @type.annotation
@@ -81,31 +97,18 @@
 ;; Property qualifier
 (prop_qualifier) @keyword.directive
 
-"+" @operator
-"-" @operator
-"*" @operator
-"/" @operator
-"%" @operator
-"^" @operator
-":=" @operator
-"=" @operator
-"<" @operator
-"<=" @operator
-"!=" @operator
-">=" @operator
-">" @operator
-"<>" @operator
+;; Operators
 (op_other) @operator
 (op_unary_other) @operator
+(bang) @operator
+(binary_expression operator: _ @operator)
 
-"(" @punctuation.bracket
-")" @punctuation.bracket
-"{" @punctuation.bracket
-"}" @punctuation.bracket
-"<" @punctuation.bracket
-">" @punctuation.bracket
+;; Punctuation
+["(" ")" "{" "}" "[" "]" "<" ">"] @punctuation.bracket
+[";" "," "." ":"] @punctuation.delimiter
 
-";" @punctuation.delimiter
-"," @punctuation.delimiter
-"." @punctuation.delimiter
-":" @punctuation.delimiter
+;; Assignment operator
+":=" @operator
+
+;; Cast operator
+"::" @operator
